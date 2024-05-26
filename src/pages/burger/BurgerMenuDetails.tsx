40790607f7f441aea2df1143e0b0702f17717b57
Developer Instinct <kendrickoppong94@gmail.com>
2024-05-26 09:01:27 -0700

    Update BurgerMenuDetails.tsx

--- src/pages/burger/BurgerMenuDetails.tsx
+++ src/pages/burger/BurgerMenuDetails.tsx
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
 import { MenuDetails as FullMenuDetails } from "..";
+import { BACKEND_URL } from "@/constants/constants";
 
 export const BurgerMenuDetails = () => {
   const { category, title, id } = useParams();
 
   return FullMenuDetails(
-    `/api/${category}/${id}`,
+    `${BACKEND_URL}/api/${category}/${id}`,
     title!,
     id
   );
